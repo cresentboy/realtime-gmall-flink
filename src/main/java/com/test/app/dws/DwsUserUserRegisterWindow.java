13fcab70f393a1d60663ed5a0946843f47545770
zhigang <fdrgdf@foxmail.com>
2022-06-10 19:15:29 +0800

    DwsTradeCartAddUuWindow

---
+++ src/main/java/com/test/app/dws/DwsUserUserRegisterWindow.java
@@ -0,0 +1,141 @@
+package com.test.app.dws;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.test.bean.UserRegisterBean;
+import com.test.utils.ClickHouseUtil;
+import com.test.utils.DateFormatUtil;
+import com.test.utils.KafkaUtil;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.AllWindowedStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+
+/**
+ * 用户域用户注册各窗口汇总表
+ *  主要任务:
+ * 	从 DWD 层用户注册表中读取数据，统计各窗口注册用户数，写入 ClickHouse。
+ *  思路分析:
+ * 1）读取 Kafka 用户注册主题数据
+ * 2）转换数据结构
+ * 	String 转换为 JSONObject。
+ * 3）设置水位线
+ * 4）开窗、聚合
+ * 5）写入 ClickHouse
+ *
+ */
+public class DwsUserUserRegisterWindow {
+    public static void main(String[] args) throws Exception {
+        // TODO 1. 环境准备
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        // TODO 2. 状态后端设置
+        env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
+        );
+        env.setRestartStrategy(RestartStrategies.failureRateRestart(
+                3, Time.days(1), Time.minutes(1)
+        ));
+        env.setStateBackend(new HashMapStateBackend());
+        env.getCheckpointConfig().setCheckpointStorage(
+                "hdfs://hadoop102:8020/ck"
+        );
+        System.setProperty("HADOOP_USER_NAME", "atguigu");
+
+        // TODO 3. 读取 Kafka dwd_user_register 主题数据，封装为流
+        String topic = "dwd_user_register";
+        String groupId = "dws_user_user_register_window";
+        FlinkKafkaConsumer<String> kafkaConsumer = KafkaUtil.getKafkaConsumer(topic, groupId);
+        DataStreamSource<String> source = env.addSource(kafkaConsumer);
+
+        // TODO 4. 转换数据结构
+        SingleOutputStreamOperator<JSONObject> mappedStream = source.map(JSON::parseObject);
+
+        // TODO 5. 设置水位线
+        SingleOutputStreamOperator<JSONObject> withWatermarkDS = mappedStream.assignTimestampsAndWatermarks(
+                WatermarkStrategy
+                        .<JSONObject>forMonotonousTimestamps()
+                        .withTimestampAssigner(
+                                new SerializableTimestampAssigner<JSONObject>() {
+                                    @Override
+                                    public long extractTimestamp(JSONObject jsonObj, long recordTimestamp) {
+                                        return jsonObj.getLong("ts") * 1000;
+                                    }
+                                }
+                        )
+        );
+
+        // TODO 6. 开窗
+        AllWindowedStream<JSONObject, TimeWindow> windowDS = withWatermarkDS.windowAll(TumblingEventTimeWindows.of(
+                org.apache.flink.streaming.api.windowing.time.Time.seconds(10L)));
+
+        // TODO 7. 聚合
+        SingleOutputStreamOperator<UserRegisterBean> aggregateDS = windowDS.aggregate(
+                new AggregateFunction<JSONObject, Long, Long>() {
+                    @Override
+                    public Long createAccumulator() {
+                        return 0L;
+                    }
+
+                    @Override
+                    public Long add(JSONObject jsonObj, Long accumulator) {
+                        accumulator += 1;
+                        return accumulator;
+                    }
+
+                    @Override
+                    public Long getResult(Long accumulator) {
+                        return accumulator;
+                    }
+
+                    @Override
+                    public Long merge(Long a, Long b) {
+                        return null;
+                    }
+                },
+                new AllWindowFunction<Long, UserRegisterBean, TimeWindow>() {
+                    @Override
+                    public void apply(TimeWindow window, Iterable<Long> values, Collector<UserRegisterBean> out) throws Exception {
+                        for (Long value : values) {
+                            String stt = DateFormatUtil.toYmdHms(window.getStart());
+                            String edt = DateFormatUtil.toYmdHms(window.getEnd());
+                            UserRegisterBean userRegisterBean = new UserRegisterBean(
+                                    stt,
+                                    edt,
+                                    value,
+                                    System.currentTimeMillis()
+                            );
+                            out.collect(userRegisterBean);
+                        }
+                    }
+                }
+        );
+
+        // TODO 8. 写入到 OLAP 数据库
+        SinkFunction<UserRegisterBean> sinkFunction = ClickHouseUtil.<UserRegisterBean>getJdbcSink(
+                "insert into dws_user_user_register_window values(?,?,?,?)"
+        );
+        aggregateDS.addSink(sinkFunction);
+
+        env.execute();
+
+    }
+}
